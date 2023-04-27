b4c210d24611fe94c15cd0024ffe4f01bf5dba54
Maria Eduarda <mariaeduardasilv916@gmail.com>
2023-04-27 09:41:36 -0300

    UsedProducts update

--- src/entities/UsedProduct.java
+++ src/entities/UsedProduct.java
@@ -1,5 +1,20 @@
 package entities;
 
+import java.util.Date;
+
 public class UsedProduct extends Product{
 
+	private Date manufactureDate;
+
+	public UsedProduct(Date manufactureDate) {
+		this.manufactureDate = manufactureDate;
+	}
+
+	public Date getManufactureDate() {
+		return manufactureDate;
+	}
+
+	public void setManufactureDate(Date manufactureDate) {
+		this.manufactureDate = manufactureDate;
+	}
 }
